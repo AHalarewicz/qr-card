fc42b211e18e713dfd59480317fdadcb3b83e97f
ahalarewicz <alhalarewicz@gmail.com>
2022-12-04 14:41:31 -0800

    write test for qrcode_generator

---
+++ tests/test_qrcode_generator.py
@@ -0,0 +1,12 @@
+from qrcard.generate_qrcode import generate_and_save_qrcode
+from os.path import exists
+
+
+
+def test_generate_and_save_qrcode():
+
+	generate_and_save_qrcode("https://github.com/AHalarewicz/qr-card",
+						  "tests/test_qrcode.png",
+						  6)
+
+	assert exists("tests/test_qrcode.png")
