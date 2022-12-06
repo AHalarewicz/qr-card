3f69fa77218c418e82b83326e0b37b11aad85e14
ahalarewicz <alhalarewicz@gmail.com>
2022-12-05 22:51:54 -0800

    get initial phone carrier for phone number

--- exploration/get_phone_carrier.py
+++ exploration/get_phone_carrier.py
@@ -1,9 +1,23 @@
-import phonenumbers
-from phonenumbers import carrier, geocoder,timezone
 
-mobileno="+13107419469"
-mobileno="+12167122456"
-mobileno="+12165331733"
-mobileno=phonenumbers.parse(mobileno)
-print(mobileno)
-print(carrier.name_for_number(mobileno,"en"))
+import requests
+
+
+def get_phone_carrier(country_code="=1", number="8888888888"):
+
+	url = "https://api.apilayer.com/number_verification/validate?number={"+country_code+number+"}"
+
+	payload = {}
+	headers= {
+	  "apikey": "OnjVIco7y6Bwjwnv2JulIlodek1QBu4K"
+	}
+
+	response = requests.request("GET", url, headers=headers, data = payload)
+
+	status_code = response.status_code
+	result = response.text
+
+	return result
+
+if __name__ == '__main__':
+	carrier = get_phone_carrier("+1", "2167122456")
+	print(carrier)
